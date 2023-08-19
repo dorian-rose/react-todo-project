c5a0871d335ee58c0003322728350cfc2d5d6705
dorian-rose <dorianrkelly@gmail.com>
2023-08-19 20:21:53 +0200

    login and register errors handled

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -20,13 +20,13 @@ export const RegisterPage = () => {
       navigate("/");
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
-        setErrors("Email already in use", error);
+        setErrors("Este email ya tiene cuenta");
       } else if (error.code === "auth/invalid-email") {
-        setErrors("Invalid email", error);
+        setErrors("Email no válido");
       } else if (error.code === "auth/weak-password") {
-        setErrors("Weak password", "error");
+        setErrors("Contraseña no segura");
       } else if (error.code) {
-        setErrors("Something went wrong", error);
+        setErrors("Algo no funciona. Inténtalo luego");
       }
     }
   };
@@ -40,6 +40,9 @@ export const RegisterPage = () => {
         <article>
           <AccessForm enterUser={enterUser} nameHidden={false} />
         </article>
+        <p className="text-alert text-sm font-thin text-center mt-4">
+          {errors}
+        </p>
         <article className="mx-4 mt-10 border-t border-slate">
           <p className="relative -top-3 left-1/2 -translate-x-1/2 bg-tertiary w-fit px-2 text-sm text-lines ">
             Regístrate con
